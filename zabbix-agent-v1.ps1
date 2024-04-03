0bc56202cdb3ffe014a22b643f6f9990b2f39931
Doğukan Eren <46019536+dogukaneren@users.noreply.github.com>
2024-04-03 15:59:18 +0300

    Update zabbix-agent-v1.ps1

--- zabbix-agent-v1.ps1
+++ zabbix-agent-v1.ps1
@@ -2,7 +2,8 @@ $server = "192.168.122.192"
 
 #Specify version here
 $version = "https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.28/zabbix_agent-6.0.28-windows-amd64-openssl.zip"
-mkdir c:\zabbix-agent
+New-Item -Path "C:\zabbix-agent" -ItemType Directory
+
 
 Invoke-WebRequest "$version" -outfile c:\zabbix-agent\zabbix.zip
 
@@ -14,11 +15,11 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Unzip "c:\Zabbix-agent\zabbix.zip" "c:\zabbix" 
+Unzip "c:\Zabbix-agent\zabbix.zip" "c:\zabbix-agent" 
 
 (Get-Content -Path c:\zabbix-agent\conf\zabbix_agentd.conf) | ForEach-Object {$_ -Replace '127.0.0.1', "$server"} | Set-Content -Path c:\zabbix-agent\conf\zabbix_agentd.conf
 
-C:\zabbix-agent\bin\zabbix_agentd.exe --config c:\zabbix\conf\zabbix_agentd.conf --install
+C:\zabbix-agent\bin\zabbix_agentd.exe --config c:\zabbix-agent\conf\zabbix_agentd.conf --install
 
 
 c:\zabbix-agent\bin\zabbix_agentd.exe --start --config C:\zabbix-agent\conf\zabbix_agentd.conf
