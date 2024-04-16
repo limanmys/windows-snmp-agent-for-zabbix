eb79db687772af3d9d21a8d2d386f195e2a9a149
Doğukan Eren <46019536+dogukaneren@users.noreply.github.com>
2024-04-16 09:49:54 +0300

    Feature VC_redist installation added to plus ps for snmpwalk

--- zabbix-agent-v1-plus.ps1
+++ zabbix-agent-v1-plus.ps1
@@ -2,10 +2,14 @@ $server = "192.168.122.192"
 
 #Specify version here
 $version = "https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.28/zabbix_agent-6.0.28-windows-amd64-openssl.zip"
+$vRedistInstallerUrl = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
 New-Item -Path "C:\zabbix-agent" -ItemType Directory
 
 
 Invoke-WebRequest "$version" -outfile c:\zabbix-agent\zabbix.zip
+Invoke-WebRequest -Uri $vRedistInstallerUrl -outfile c:\zabbix-agent\vc_redist.x64.exe
+c:\zabbix-agent\vc_redist.x64.exe /quiet /norestart
+Remove-Item "c:\zabbix-agent\vc_redist.x64.exe"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
