004ef6832a9e9fc2bc36e2b3f9efc77a3c748d31
Doğukan Eren <46019536+dogukaneren@users.noreply.github.com>
2024-04-17 16:57:35 +0300

    Update snmpv2-request.ps1

--- Scripts/snmpv2-request.ps1
+++ Scripts/snmpv2-request.ps1
@@ -4,7 +4,7 @@ param (
     [string]$oid
 )
 
-$result = snmpwalk -v2c -c $community $ipAddress $oid
+$result = snmpwalk -v2c -c $community $ipAddress $oid 2>$null
 $response = $result -replace '.*STRING:\s*'
 
 Write-Output $response
