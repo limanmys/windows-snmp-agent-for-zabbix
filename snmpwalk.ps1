5b416f96e444a74a2a0a493d1e9809d1481afe98
Doğukan Eren <46019536+dogukaneren@users.noreply.github.com>
2024-04-19 07:21:50 +0300

    Feature: Slient download and install added to snmpwalk.ps1
    Downloads form releases then ads to environment

--- snmpwalk.ps1
+++ snmpwalk.ps1
@@ -1,7 +1,22 @@
-
 $currentPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
 
-$snmpwalkDirectory = "C:\Program Files\"
+$zabbixAgentDirectory = "C:\Program Files\zabbix-agent"
+$snmpwalkDirectory = "$zabbixAgentDirectory"
+
+if (-not (Test-Path -Path $zabbixAgentDirectory)) {
+    New-Item -ItemType Directory -Path $zabbixAgentDirectory | Out-Null
+}
+
+$snmpwalkDownloadUrl = "https://github.com/limanmys/zabbix-agent-slient/releases/download/snmp/snmpwalk.exe"
+$snmpwalkFilePath = "$zabbixAgentDirectory\snmpwalk.exe"
+
+if (-not (Test-Path -Path $snmpwalkFilePath)) {
+    Invoke-WebRequest -Uri $snmpwalkDownloadUrl -OutFile $snmpwalkFilePath
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to download snmpwalk.exe. Exiting."
+        exit 1
+    }
+}
 
 if ($currentPath -notlike "*$snmpwalkDirectory*") {
     [Environment]::SetEnvironmentVariable("Path", "$currentPath;$snmpwalkDirectory", "Machine")
